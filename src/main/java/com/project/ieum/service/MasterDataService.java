531e9fe809193fd06035dbe9cdb71f6306f78c8f
ligr00vefe <ligr00vefe@naver.com>
2026-05-17 06:55:36 +0900

    build: service, config, controller, build.gradle 수정

---
+++ src/main/java/com/project/ieum/service/MasterDataService.java
@@ -0,0 +1,41 @@
+package com.project.ieum.service;
+
+import com.project.ieum.entity.CommunicationMethod;
+import com.project.ieum.entity.PersonalityTag;
+import com.project.ieum.entity.Region;
+import com.project.ieum.entity.user.DisabilityType;
+import com.project.ieum.repository.*;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MasterDataService {
+
+    private final DisabilityTypeRepository disabilityTypeRepository;
+    private final CommunicationMethodRepository communicationMethodRepository;
+    private final PersonalityTagRepository personalityTagRepository;
+    private final RegionRepository regionRepository;
+
+    public List<DisabilityType> getAllDisabilityTypes() {
+        return disabilityTypeRepository.findAllByOrderBySortOrderAsc();
+    }
+
+    public List<CommunicationMethod> getAllCommunicationMethods() {
+        return communicationMethodRepository.findAllByOrderBySortOrderAsc();
+    }
+
+    public List<PersonalityTag> getAllPersonalityTags() {
+        return personalityTagRepository.findAllByOrderByIdAsc();
+    }
+
+    public List<Region> getAllRegions() {
+        return regionRepository.findAllByOrderBySidoAscSigunguAscDongAsc();
+    }
+}
