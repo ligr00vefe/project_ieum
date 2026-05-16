99341947022775d672b22c3c8d679b3b58a246c0
ligr00vefe <ligr00vefe@naver.com>
2026-05-17 06:45:38 +0900

    build: DTO setting

---
+++ src/main/java/com/project/ieum/dto/ActivityInfoDTO.java
@@ -0,0 +1,29 @@
+package com.project.ieum.dto;
+
+import lombok.*;
+
+import java.time.LocalTime;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ActivityInfoDTO {
+
+    private String experience;
+
+    private List<AvailabilityTimeDTO> availabilityTimes;
+
+    private List<Long> regionIds;
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class AvailabilityTimeDTO {
+        private Short dayOfWeek; // 0-6 (일요일-토요일)
+        private LocalTime startTime;
+        private LocalTime endTime;
+    }
+}
